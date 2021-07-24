05e8c5e3369e415875fe0bd4ab354a14b81e6d7f
Kira Burova <burovakira11@gmail.com>
2021-07-24 18:34:34 +0300

    add global styles

---
+++ src/global-styles.js
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+    *, *:before, *:after {
+        box-sizing: border-box;
+    }
+    html, body {
+        height: 100%;
+        font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+    }
+    #root {
+        height: 100%;
+        font-size: 1rem;
+    }
+`;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { GlobalStyles } from './global-styles';
 import 'normalize.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <>
+    <GlobalStyles />
+    <App />
+  </>,
+  document.getElementById('root'),
+);
