6420d7185aac0eb9372ec707906069f40d07a402
Kira Burova <burovakira11@gmail.com>
2021-08-30 20:39:26 +0300

    removed broken tests, updated button test

--- src/__tests__/button.test.js
+++ src/__tests__/button.test.js
@@ -6,9 +6,15 @@ import Button from '../components/Button';
 
 describe('<Button />', () => {
   it('renders', () => {
-    const { container, getByText } = render(<Button>Click me</Button>);
+    const { container, queryByText } = render(<Button>Click me</Button>);
 
-    expect(getByText('Click me')).toBeTruthy();
+    expect(queryByText('Click me')).toBeTruthy();
+    expect(container.firstChild).toMatchSnapshot();
+  });
+  it('shows loader', () => {
+    const { container, queryByText } = render(<Button isLoading={true}>Click me</Button>);
+
+    expect(queryByText('Click me')).toBeFalsy();
     expect(container.firstChild).toMatchSnapshot();
   });
 });
